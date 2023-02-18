296df29772ca38196e540b4abeca9ae745d27276
sgazobi <sgazobi@gmail.com>
2023-02-18 01:08:18 +0100

    init

---
+++ swapValues.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 20;
+temp = a;
+a = b;
+b = temp;
+
+let nouvellevaleurdea = "new value of a:";
+let nouvellevaleurdeb = "new value of b:";
+
+console.log (nouvellevaleurdea + a);
+console.log (nouvellevaleurdeb + b);
